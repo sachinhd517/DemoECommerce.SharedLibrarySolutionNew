af2b616fca935d77fd243fe96374370c686e4763
sachin hedaoo <sachinhd517@gmail.com>
2025-01-26 19:39:14 +0530

    Refactor Response namespace and add IGenericInterface
    Updated the namespace in `Response.cs` from
    `eCommerce.SharedLibrary.Response` to
    `eCommerce.SharedLibrary.Responses`. Defined a new
    record `Response` with properties `Flag` and `Message`.
    
    Enhanced `IGenericInterface.cs` with necessary using
    directives and introduced a new generic interface
    `IGenericInterface<T>`, which includes asynchronous
    methods for CRUD operations and entity retrieval.

--- eCommerce.SharedLibrary/Response/Response.cs
+++ eCommerce.SharedLibrary/Response/Response.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace eCommerce.SharedLibrary.Response
+namespace eCommerce.SharedLibrary.Responses
 {
     public record Response (bool Flag = false, string Message = null!);
 }
