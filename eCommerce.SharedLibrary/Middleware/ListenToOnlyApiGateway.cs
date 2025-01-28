7f8ed71f51f2a202249fe7abfba54dad9781b6e9
sachin hedaoo <sachinhd517@gmail.com>
2025-01-28 15:59:50 +0530

    Add ListenToOnlyApiGateway middleware class
    Introduces the `ListenToOnlyApiGateway` class to handle HTTP requests by checking for the `API-Gateway OR X-Api-Gateway-Signature` header. If the header is missing, it responds with a 503 Service Unavailable status; otherwise, it invokes the next middleware in the pipeline. Utilizes `HttpContext` for request and response management.

---
+++ eCommerce.SharedLibrary/Middleware/ListenToOnlyApiGateway.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eCommerce.SharedLibrary.Middleware
+{
+    public class ListenToOnlyApiGateway(RequestDelegate contexxt)
+    {
+        public async Task InvokeAsync(HttpContext context)
+        {
+            // Extract specific header from the request
+            var signedHeader = context.Request.Headers["API-Gateway OR X-Api-Gateway-Signature"];
+
+            // Null means, the request is not coming from the API Gateway // 503 service unavailable
+            if(signedHeader.FirstOrDefault() == null)
+            {
+                context.Response.StatusCode = StatusCodes.Status503ServiceUnavailable;
+                await context.Response.WriteAsync("Service Unavailable");
+            }
+            else
+            {
+                await contexxt(context);
+            }
+
+        }
+    }
+    
+}
