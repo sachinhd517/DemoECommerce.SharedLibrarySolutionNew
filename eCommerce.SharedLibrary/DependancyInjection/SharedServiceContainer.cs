23e493b9df1d438b9b027b0f55daa0a3b605fdf7
sachin hedaoo <sachinhd517@gmail.com>
2025-01-28 18:40:26 +0530

    Add SharedServiceContainer for DI and middleware setup
    Introduces a new static class `SharedServiceContainer` in the `eCommerce.SharedLibrary.DependancyInjection` namespace. This class includes methods for configuring dependency injection with a SQL Server context, setting up Serilog for logging, and adding JWT authentication. It also implements middleware for global exception handling and restricts API access to the API gateway. Additional using directives are included for necessary namespaces.

---
+++ eCommerce.SharedLibrary/DependancyInjection/SharedServiceContainer.cs
@@ -0,0 +1,53 @@
+﻿using eCommerce.SharedLibrary.Middleware;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eCommerce.SharedLibrary.DependancyInjection
+{
+    public static class SharedServiceContainer
+    {
+        public static IServiceCollection AddSharedService<TContext>
+            (this IServiceCollection service, IConfiguration config, string fileName) where TContext : DbContext
+        {
+            service.AddDbContext<TContext>(options =>
+
+                options.UseSqlServer(config.GetConnectionString("eCommerceConnection"), sqlserverOption => sqlserverOption.EnableRetryOnFailure()));
+
+            //  configuration serilog logging
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Information()
+                .WriteTo.Debug()
+                .WriteTo.Console()
+                .WriteTo.File(path: $"{fileName}-.text",
+                restrictedToMinimumLevel: Serilog.Events.LogEventLevel.Information,
+                outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {message:lj} {newLine} {Exception}",
+                rollingInterval: RollingInterval.Day)
+                .CreateLogger();
+
+
+            // Add JWT Authentication Schema
+            JWTAuthenticationSchema.AddJWTAuthenticationSchema(service, config);
+            return service;
+        }
+
+        public static IApplicationBuilder UseSharedPolicies(this IApplicationBuilder app)
+        {
+            // Use golobal Exception
+            app.UseMiddleware<GobalException>();
+
+            // Register middleware to block all outsiders API calls
+            app.UseMiddleware<ListenToOnlyApiGateway>();
+
+            return app;
+        }
+    }
+    
+}
