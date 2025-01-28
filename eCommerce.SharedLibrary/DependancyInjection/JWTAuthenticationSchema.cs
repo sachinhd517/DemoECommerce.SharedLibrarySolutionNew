9cc273bc2088e74abb888c0299a55f08e7601eaa
sachin hedaoo <sachinhd517@gmail.com>
2025-01-28 16:18:51 +0530

    Add JWT authentication configuration class
    Introduces `JWTAuthenticationSchema` class to configure JWT bearer authentication in the `eCommerce.SharedLibrary.DependancyInjection` namespace. The new method `AddJWTAuthenticationSchema` extends `IServiceCollection` to set up authentication using configuration settings for the key, issuer, and audience. It also defines token validation parameters, enhancing security for the application.

---
+++ eCommerce.SharedLibrary/DependancyInjection/JWTAuthenticationSchema.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eCommerce.SharedLibrary.DependancyInjection
+{
+    public static class JWTAuthenticationSchema
+    {
+        public static IServiceCollection AddJWTAuthenticationSchema(this IServiceCollection services, IConfiguration config)
+
+        {
+            // Add JWT Authentication Schema
+            services.AddAuthentication("Bearer")
+                .AddJwtBearer("Bearer", options =>
+                {
+                    var key = Encoding.UTF8.GetBytes(config.GetSection("Authentication:key").Value!);
+                    string issuer = config.GetSection("Authentication:Issuer").Value!;
+                    string audience = config.GetSection("Authentication:Audience").Value!;
+
+                    options.RequireHttpsMetadata = false;
+                    options.SaveToken = true;
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidIssuer = issuer,
+                        ValidateAudience = true,
+                        ValidateLifetime = false,
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new Microsoft.IdentityModel.Tokens.SymmetricSecurityKey(key),
+                        //ClockSkew = TimeSpan.Zero
+                    };
+                });
+            return services;
+        }
+    }
+}
