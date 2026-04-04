cad77c03e1390895f3e3264929b1133f66140cfa
sachinhd517 <sachinhd517@gmail.com>
2026-04-04 14:16:02 +0530

    LogException file add

---
+++ eCommerce.SharedLibrary/Logs/LogException.cs
@@ -0,0 +1,33 @@
+﻿using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eCommerce.SharedLibrary.Logs
+{
+    public static class LogException
+    {
+        public static void LogExceptions(Exception ex)
+        {
+            LogToFile(ex.Message);
+            LogToCnsole(ex.Message);
+            LogToDebugger(ex.Message);
+        }
+
+        private static void LogToFile(string message)
+        {
+            Log.Information(message);
+        }
+        private static void LogToDebugger(string message)
+        {
+            Log.Debug(message);
+        }
+
+        private static void LogToCnsole(string message)
+        {
+            Log.Warning(message);
+        }
+    }
+}
